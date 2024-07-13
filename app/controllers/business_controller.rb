cc9ae45fc547318b6d9a2aa13375450f7d703c92
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-13 13:10:20 -0400

    Implement index and create controller logic

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,3 +1,36 @@
 class BusinessController < ApplicationController
-  def index; end
+  include Rails.application.routes.url_helpers
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def index
+    @businesses = Business.all
+
+    if @businesses.empty?
+      render json: { error: 'No business found' }, status: :not_found
+    else
+      render json: businesses_with_images(@businesses)
+    end
+  end
+
+  def create
+    @business = Business.new(business_params)
+
+    if @business.save
+      render json: @business, status: :created
+    else
+      render json: @business.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def businesses_with_images(businesses)
+    businesses.map do |business|
+      business.as_json.merge(image_url: business.image.attached? ? url_for(business.image) : nil)
+    end
+  end
+
+  def business_params
+    params.require(:business).permit(:business_name, :owners_name, :business_description, :location, :contact, :image)
+  end
 end
