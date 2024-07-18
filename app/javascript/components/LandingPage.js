08ea88126be493de8f36b32999967f7f5c34400c
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-18 05:06:40 -0400

    Add background image

--- app/javascript/components/LandingPage.js
+++ app/javascript/components/LandingPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Business from './Business';
+import NavBar from './NavBar';
+import Hero from './Hero';
+import wave from '../../assets/images/wave.svg';
 
 const LandingPage = () => (
   <div>
+    <div className="bg-hero-container" style={{ backgroundImage: `url(${wave})` }}>
+      <NavBar />
+      <Hero />
+    </div>
     <Business />
   </div>
 );
