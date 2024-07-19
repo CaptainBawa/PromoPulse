6beabf74ce1062b9862f7ac06b34fcda5eb138c3
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-19 14:08:25 -0400

    Change div to section

---
+++ app/javascript/components/AboutAddBuz.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AboutAddBiz = () => (
+  <section className="about-business">
+    <div className="about-us">
+      <h2>About Us</h2>
+      <p>
+        At our site, we strive to uplift and promote businesses by providing
+        a space to showcase talents and services. Our mission is to support
+        the growth and success of budding entrepreneurs by connecting them
+        with a global audience. Join us on this journey of creativity,
+        innovation, and success!
+      </p>
+    </div>
+    <div className="add-buz">
+      <h2>Add your business</h2>
+      <p>
+        Interested in adding your business for free? Contact us for validation
+        and get featured on our platform. We are here to help you reach a global
+        audience and grow your business.
+      </p>
+    </div>
+  </section>
+);
+
+export default AboutAddBiz;
