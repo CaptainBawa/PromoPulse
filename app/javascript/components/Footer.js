38e9e902b8adefaf4969315fe52588e61fc1b3a2
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-19 14:08:48 -0400

    Change text

---
+++ app/javascript/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <div className="marquee">
+      <p>
+        Contact: +233546006938. Need a website, web app, or management
+        system for your business? Reach out to me for professional
+        development services!
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
