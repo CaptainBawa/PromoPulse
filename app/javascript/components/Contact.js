650b74577f094e91a6dd37a84871e68748fc8975
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-19 09:03:58 -0400

    Add contact us section

---
+++ app/javascript/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <p>
+      If you have any questions or want to add
+      your business,feel free to reach out to us:
+    </p>
+    <h3>0549542885</h3>
+    <h3>0549542885</h3>
+  </div>
+);
+
+export default Contact;
