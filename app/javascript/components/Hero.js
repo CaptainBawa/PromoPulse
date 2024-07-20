6a74330aa8b21e7b9418e071780eff4dfbf61881
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-20 15:01:43 -0400

    Reposition hero page image

--- app/javascript/components/Hero.js
+++ app/javascript/components/Hero.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import business from '../../assets/images/business.png';
-import globe from '../../assets/images/globe.png';
 
 const Hero = () => (
   <section className="hero-container">
     <div>
+      <p>
+        We empower entrepreneurs by providing a dynamic platform
+        where they can showcase their businesses, connect with
+        their customers, and drive growth through meaningful
+        engagement and support.
+      </p>
       <img src={business} alt={business} />
-      <img src={globe} alt={globe} />
     </div>
-    <p>
-      We empower entrepreneurs by providing a dynamic platform
-      where they can showcase their businesses, connect with
-      their customers, and drive growth through meaningful
-      engagement and support.
-    </p>
   </section>
 );
 
