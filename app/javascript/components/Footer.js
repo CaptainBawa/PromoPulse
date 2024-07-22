bf11bcf3ec37baeabdcc181ace8bb62542c27336
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-21 20:00:21 -0400

    Add dot

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
       <p>
         Contact: +233546006938. Need a website, web app, or management
         system for your business? Reach out to me for professional
-        development services!
+        development services.
       </p>
     </div>
   </footer>
