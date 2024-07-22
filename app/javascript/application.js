49b8c618e8e8f169853bd9b20eb1d354ba85e440
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-22 14:45:15 -0400

    Build again

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
-// import "controllers"
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
